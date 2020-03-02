8130e1b12ea1f2090b243bc37eb8770e3325aa1e
shaik ammajan <sammajanb@gmail.com>
2020-03-02 15:32:48 +0530

    gg

---
+++ buildf/precache-manifest.6d65028713922fcd2613fadceec15797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2066a4feda4e15e7e41e2d8addaad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fa3bfa00a3faf2f1302",
+    "url": "/static/css/main.e51d89b1.chunk.css"
+  },
+  {
+    "revision": "89e757439796368759a0",
+    "url": "/static/js/2.37d5a482.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.37d5a482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa3bfa00a3faf2f1302",
+    "url": "/static/js/main.22eba952.chunk.js"
+  },
+  {
+    "revision": "d8318b7374465af13d19",
+    "url": "/static/js/runtime-main.fd2ae6f7.js"
+  }
+]);
